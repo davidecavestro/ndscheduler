a62d0fac22743ff2803e28c8ba3b357a361b9873
Davide Cavestro <davide.cavestro@gmail.com>
2018-11-12 06:38:13 +0100

    Override persistence settings

--- simple_scheduler/settings.py
+++ simple_scheduler/settings.py
@@ -10,7 +10,29 @@ DEBUG = True
 HTTP_PORT = 8888
 HTTP_ADDRESS = '0.0.0.0'
 
+# Rome TZ
 TIMEZONE = 'Europe/Rome'
+
+
+# Override persistence settings
+
+# SQLite
+#
+DATABASE_CLASS = 'ndscheduler.core.datastore.providers.sqlite.DatastoreSqlite'
+DATABASE_CONFIG_DICT = {
+    'file_path': '/app/data/datastore.db'
+}
+
+# MySQL
+#
+# DATABASE_CLASS = 'ndscheduler.core.datastore.providers.mysql.DatastoreMysql'
+# DATABASE_CONFIG_DICT = {
+#     'user': 'ndscheduler',
+#     'password': 'password',
+#     'hostname': 'mysql',
+#     'port': 3306,
+#     'database': 'scheduler'
+# }
 
 #
 # Set logging level
